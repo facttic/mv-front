5825672986e4267508b65e06c9100214a245ef05
Ignacio Guerra <ignacio.guerra@nayra.coop>
2020-03-22 23:13:33 -0300

    Favicon incluido

--- src/containers/App.js
+++ src/containers/App.js
@@ -292,7 +292,10 @@ class App extends Component {
           </Grid>
           {preloader}
           {tweetCard}
-          <Footer><Link href="https://facttic.org.ar/" target="_blank" rel="noopener noreferrer">Desarrollado por FACTTIC</Link></Footer>
+          <Footer>
+            {/* <img src="/favicon.png" width="48" alt="Marcha Virtual" /> */}
+            <Link href="https://facttic.org.ar/" target="_blank" rel="noopener noreferrer">Desarrollado por FACTTIC</Link>
+          </Footer>
           <Route path="/moderar">
             <Overlay/>
             <Login />
