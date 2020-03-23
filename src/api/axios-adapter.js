565986e46dabc4ce2fdd77a47d70eb49069bde9a
florblue <florencia.otarola@gmail.com>
2020-03-23 18:37:32 -0300

    Add users counter logic

--- src/api/axios-adapter.js
+++ src/api/axios-adapter.js
@@ -1,4 +1,4 @@
-import { UserTypes } from '../constants'
+import { UserTypes } from "../constants";
 
 export default (client, options = {}) => {
   return (type, params) => {
@@ -8,7 +8,8 @@ export default (client, options = {}) => {
       AUTH_CHECK_REQUEST,
       USERS_BAN_REQUEST,
       USERS_DELETE_TWEET_REQUEST,
-    } = UserTypes
+      USERS_COUNT_REQUEST
+    } = UserTypes;
 
     const {
       authFields,
@@ -16,14 +17,17 @@ export default (client, options = {}) => {
       tokenField,
       tokenStorageKey,
       userField,
-      userStorageKey,
-    } = Object.assign({
-      authFields: { username: 'username', password: 'password' },
-      tokenField: 'token',
-      tokenStorageKey: 'token',
-      userField: 'user',
-      userStorageKey: 'user',
-    }, options);
+      userStorageKey
+    } = Object.assign(
+      {
+        authFields: { username: "username", password: "password" },
+        tokenField: "token",
+        tokenStorageKey: "token",
+        userField: "user",
+        userStorageKey: "user"
+      },
+      options
+    );
 
     switch (type) {
       /**
@@ -32,72 +36,71 @@ export default (client, options = {}) => {
       case AUTH_LOGIN_REQUEST:
         return new Promise((resolve, reject) => {
           const headers = {
-            'Content-Type': 'application/json',
-          }
+            "Content-Type": "application/json"
+          };
           const data = {
             [authFields.username]: params.username,
             [authFields.password]: params.password
-          }
+          };
 
-          const method = 'post'
-          const url = `${authUrl}/users/login`
+          const method = "post";
+          const url = `${authUrl}/users/login`;
           client({ url, headers, data, method })
             .then(response => {
-              const { data } = response
-              const { [tokenField]: token, [userField]: user } = data
-              localStorage.setItem(tokenStorageKey, token)
-              localStorage.setItem(userStorageKey, JSON.stringify(user))
-              client.defaults.headers.common['Authorization'] = token
-              console.log(user)
-              resolve(user)
+              const { data } = response;
+              const { [tokenField]: token, [userField]: user } = data;
+              localStorage.setItem(tokenStorageKey, token);
+              localStorage.setItem(userStorageKey, JSON.stringify(user));
+              client.defaults.headers.common["Authorization"] = token;
+              console.log(user);
+              resolve(user);
             })
             .catch(error => {
-              localStorage.removeItem(tokenStorageKey)
-              reject(error)
-            })
-        })
+              localStorage.removeItem(tokenStorageKey);
+              reject(error);
+            });
+        });
 
       case AUTH_LOGOUT_REQUEST:
         return new Promise(resolve => {
-          const token = localStorage.getItem(tokenStorageKey)
+          const token = localStorage.getItem(tokenStorageKey);
           const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            token,
-          }
-          const method = 'post'
-          const url = `${authUrl}/users/me/logout`
-          client({ url, headers, method })
-            .then(() => {
-              localStorage.removeItem(tokenStorageKey)
-              localStorage.removeItem(userStorageKey)
-              delete client.defaults.headers.common['Authorization']
-            })
-          resolve()
-        })
-        
+            "Content-Type": "application/x-www-form-urlencoded",
+            token
+          };
+          const method = "post";
+          const url = `${authUrl}/users/me/logout`;
+          client({ url, headers, method }).then(() => {
+            localStorage.removeItem(tokenStorageKey);
+            localStorage.removeItem(userStorageKey);
+            delete client.defaults.headers.common["Authorization"];
+          });
+          resolve();
+        });
+
       case AUTH_CHECK_REQUEST:
         return new Promise((resolve, reject) => {
-          const token = localStorage.getItem(tokenStorageKey)
+          const token = localStorage.getItem(tokenStorageKey);
           if (token) {
-            const url = authUrl
+            const url = authUrl;
             const headers = {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              token,
-            }
-            const method = 'get'
+              "Content-Type": "application/x-www-form-urlencoded",
+              token
+            };
+            const method = "get";
             client({ url, headers, method })
               .then(response => {
-                const { data } = response
-                const { [userField]: user } = data
-                resolve(user)
+                const { data } = response;
+                const { [userField]: user } = data;
+                resolve(user);
               })
               .catch(error => {
-                reject(error)
-              })
+                reject(error);
+              });
           } else {
-            reject('Authentication failed.')
+            reject("Authentication failed.");
           }
-        })
+        });
 
       /**
        * Other network resources. This could be moved elsewhere - @sgobotta
@@ -105,43 +108,61 @@ export default (client, options = {}) => {
 
       case USERS_BAN_REQUEST:
         return new Promise((resolve, reject) => {
-          const token = localStorage.getItem(tokenStorageKey)
+          const token = localStorage.getItem(tokenStorageKey);
           const headers = {
-            'Content-Type': 'application/json',
-            token,
-          }
-          const method = 'post'
-          const data = { "user_id_str": params.userTwitterId }
-          const url = `${authUrl}/blacklists`
+            "Content-Type": "application/json",
+            token
+          };
+          const method = "post";
+          const data = { user_id_str: params.userTwitterId };
+          const url = `${authUrl}/blacklists`;
           client({ url, data, headers, method })
             .then(res => {
-              resolve(res)
+              resolve(res);
             })
             .catch(error => {
-              reject(error)
+              reject(error);
+            });
+        });
+
+      case USERS_COUNT_REQUEST:
+        return new Promise((resolve, reject) => {
+          const token = localStorage.getItem(tokenStorageKey);
+          const headers = {
+            "Content-Type": "application/json",
+            token
+          };
+          const method = "get";
+          const url = `${authUrl}/twitter_users`;
+          client({ url, headers, method })
+            .then(res => {
+              resolve(res);
             })
-        })
+            .catch(error => {
+              reject(error);
+            });
+        });
 
       case USERS_DELETE_TWEET_REQUEST:
         return new Promise((resolve, reject) => {
-          const token = localStorage.getItem(tokenStorageKey)
+          const token = localStorage.getItem(tokenStorageKey);
           const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            token,
-          }
-          const method = 'delete'
-          const url = `${authUrl}/tweets/${params.tweetId}`
+            "Content-Type": "application/x-www-form-urlencoded",
+            token
+          };
+          const method = "delete";
+          const url = `${authUrl}/tweets/${params.tweetId}`;
           client({ url, headers, method })
             .then(res => {
-              resolve(res)
+              resolve(res);
             })
             .catch(error => {
-              reject(error)
-            })
-        })
+              reject(error);
+            });
+        });
 
       default:
         return Promise.reject(`Unsupported action type: ${type}`);
     }
-  }
-}
+  };
+};
--- src/api/index.js
+++ src/api/index.js
@@ -1,38 +1,43 @@
-import axios from 'axios'
-import createAxiosAdapter from './axios-adapter'
-import { UserTypes } from '../constants'
-const { REACT_APP_API_URL: API_URL } = process.env
+import axios from "axios";
+import createAxiosAdapter from "./axios-adapter";
+import { UserTypes } from "../constants";
+const { REACT_APP_API_URL: API_URL } = process.env;
 
 export const providerOptions = {
-  authFields: { username: 'email', password: 'password' },
+  authFields: { username: "email", password: "password" },
   authUrl: API_URL,
-  tokenStorageKey: 'marchavirtual.token',
-  userStorageKey: 'marchavirtual.user',
-  tokenField: 'token',
-  userfield: 'user'
-}
+  tokenStorageKey: "marchavirtual.token",
+  userStorageKey: "marchavirtual.user",
+  tokenField: "token",
+  userfield: "user"
+};
 
-const provider = createAxiosAdapter(axios, providerOptions)
+const provider = createAxiosAdapter(axios, providerOptions);
 
 const {
   AUTH_LOGIN_REQUEST,
   AUTH_LOGOUT_REQUEST,
   USERS_BAN_REQUEST,
-  USERS_DELETE_TWEET_REQUEST
-} = UserTypes
+  USERS_DELETE_TWEET_REQUEST,
+  USERS_COUNT_REQUEST
+} = UserTypes;
 
 export default {
   users: {
-    login: (username, password) => provider(AUTH_LOGIN_REQUEST, {
-      username,
-      password,
-    }),
+    login: (username, password) =>
+      provider(AUTH_LOGIN_REQUEST, {
+        username,
+        password
+      }),
     logout: () => provider(AUTH_LOGOUT_REQUEST),
-    banUser: userTwitterId => provider(USERS_BAN_REQUEST, {
-      userTwitterId
-    }),
-    deleteTweet: tweetId => provider(USERS_DELETE_TWEET_REQUEST, {
-      tweetId
-    })
+    banUser: userTwitterId =>
+      provider(USERS_BAN_REQUEST, {
+        userTwitterId
+      }),
+    deleteTweet: tweetId =>
+      provider(USERS_DELETE_TWEET_REQUEST, {
+        tweetId
+      }),
+    usersCount: () => provider(USERS_COUNT_REQUEST)
   }
-}
+};
