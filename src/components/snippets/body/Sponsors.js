85dd6bd89e6f633a986675741e0c64c1f9a85251
Romi Gomez <romina.gomez@nayra.coop>
2021-03-03 12:51:07 -0300

    merge sponsors + header background

--- src/components/snippets/body/Sponsors.js
+++ src/components/snippets/body/Sponsors.js
@@ -15,20 +15,36 @@ justify-content: center;
 margin-top: 60px;
 `;
 
+const TextContainer = styled.p`
+font-family: ${props => props.theme.styles.colors.accent};
+justify-content: center;
+display: flex;
+`;
+
+const ImagesContainer = styled.div`
+justify-content: center;
+display: flex;
+margin-top: 24px;
+`;
+
 const Image = styled.img`
-max-width: 250px;
+max-height: 100px;
 `;
 
 const Sponsors = (props) => {
     return (
         <SponsorsContainer>
-            {props.sponsors.map(sponsor => <Link
-                href={sponsor.pageUri}
-                target="_blank"
-                rel="noopener noreferrer">
-                <Image alt={sponsor.name} src={sponsor.logoUri}></Image>
-            </Link>)}
-
+            <div>
+                <TextContainer> Nos acompañan en esta marcha </TextContainer>
+                <ImagesContainer>
+                    {props.sponsors.map(sponsor => <Link
+                        href={sponsor.pageUri}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <Image alt={sponsor.name} src={sponsor.logoUri}></Image>
+                    </Link>)}
+                </ImagesContainer>
+            </div>
         </SponsorsContainer>
     );
 };
