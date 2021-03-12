26f2d7b21881cecad5ce8abeea180637feee87d9
Romi Gomez <romina.gomez@nayra.coop>
2021-03-12 14:13:42 -0300

    default header + preloader

--- src/components/snippets/body/Preloader.js
+++ src/components/snippets/body/Preloader.js
@@ -4,13 +4,14 @@ import styled from "styled-components";
 const Center = styled.div`
   text-align: center;
   padding: 3em;
+  width:50px;
 `;
 
 export default function Preloader({
     url
 }) {
     return <Center>
-        <img src={url || require("../../../assets/imgs/spinner.gif")} alt="Cargando" />
+        <img src={url || require("../../../assets/imgs/loading.gif")} alt="Cargando" />
     </Center>
 }
 
