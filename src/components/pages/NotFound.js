c7ec6c1e9b1b9a31db8d1aab2be82b6ce46971ec
Agustina Sanchez <agustinabelen.sanchez@gmail.com>
2021-03-15 17:52:16 -0300

    estilos pantalla not found + imagen de fondo facttic/mv-issues #89

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,31 @@
 import React from 'react';
-const notfoundBackground= require('../../assets/imgs/notfound.jpg')
+import { useHistory } from 'react-router';
+import styled, { withTheme } from 'styled-components'
+
+
+const notfoundBackground = require('../../assets/imgs/notfound.jpg')
+
+const Button = styled.a`
+  border-radius: 8px;
+  padding: 10px;
+  width: 11rem;
+  background-color: orange;
+  color: black;
+`;
 
 
 const NotFound = () => {
+    const history = useHistory()
+
     return (
-        <div style={{display:"flex", justifyContent:"center"}}>
-            <h1 style={{position:"absolute"}}>No encontramos una marcha</h1>
-            <img src={notfoundBackground}/> 
+        <div style={{ textAlign: "center" }}>
+            <div style={{ position: "relative", marginTop: "24px" }}>
+            <h1> No encontramos ninguna marcha con ese nombre</h1>
+            <Button href="#" onClick={() => history.push("/")}>
+                Volver a Marchas Virtuales
+            </Button>
+            </div>
+            <img src={notfoundBackground} />
         </div>
     );
 };
